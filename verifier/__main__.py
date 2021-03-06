1393dd38e3ab423b05d5dfceddbddb5aa44794d9
Vasis3038 <43793237+Vasis3038@users.noreply.github.com>
2021-03-06 17:25:30 +0300

    Update __main__.py

--- verifier/__main__.py
+++ verifier/__main__.py
@@ -1,32 +1,29 @@
 import requests
 import json
 
-TOKEN = f'99c16269c55c64669c0f6ec5a77bd4be0c0'
+TOKEN = f'd89f3c9bb2c3cf2e4f7a7b16e5811a00716ebbfb'
 headers = {
     "Accept": "application/vnd.github.v3+json",
     "Authorization": TOKEN,
     }
 
-PREFIXES = ['GENERATOR', 'LEETCODE', 'HEXNUMBER', 'TRIANGLE', 'ITERATOR', 'VERIFIER']
+PREFIXES = ['GENERATOR', 'LEETCODE', 'HEXNUMBER', 'TRIANGLE', 'ITERATOR', 'REQUESTS']
 GROUPS = ['1021', '1022']
 
-def read_file():
-    f = open('links.txt', 'r')
-    file = f.readlines()
-    f.close()
-    return file
-
-def check_title(pr):
-    title = pr['title'].split()
+def check_prefixes(title):
+    title = title.split()
     pref = title[0].split('-')
     if len(pref) != 2:
         return False
     group = pref[1]
     pref = pref[0]
-    if group in GROUPS and pref in PREFIXES:
-        return True
+    if group in GROUPS:
+        if pref in PREFIXES:
+            return 'OKK'
+        else:
+            return 'WRONG PREFIX'
     else:
-        return False
+        return 'WRONG GROUP'
 
 def get_all_user_pr(user_login, repo_name, pr_state):
     user_login = 'https://api.github.com/repos/' + user_login
@@ -39,15 +36,20 @@ def get_all_user_pr(user_login, repo_name, pr_state):
     return res
 
 def get_all_commits(pr):
-  print(pr['commits_url'])
+  #print(pr['commits_url'])
   res = []
-  message = requests.get(pr['commits_url'])
-  for commit in message.json():
+  link = requests.get(pr['commits_url'])
+  for commit in link.json():
     res.append(commit['commit'])
   return res
+
+#def send_pr_comment(pr, errors):
+
 
 
 
 if __name__ == '__main__':
-    file = read_file()
-    print_commits(file)
+    f = 'alexarlord-boop'
+    repo_name = 'python_au'
+    pr_state = 'open'
+    print(get_all_user_pr(f, repo_name, pr_state))
